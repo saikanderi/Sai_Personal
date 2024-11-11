deaf794a2ddc37e691af5502320a4dee9798ee61
saikanderi <sairamkanderi2001@gmail.com>
2024-11-12 04:37:33 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class evenodd {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("Enter number");
+        int x = scanner.nextInt();
+        if(x%2==0)
+        System.out.println("even");
+        else
+        System.out.println("odd");
+        scanner.close();
+    }
+}
+
